49e308a4909d0e9a56a03efae297e59e955b448d
Collin Jilbert <cjilbert504@gmail.com>
2026-04-29 09:02:30 -0500

    add CSS/design

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -4,4 +4,16 @@ class Budget < ApplicationRecord
   def formatted_month
     month.strftime("%B %Y")
   end
+
+  def total_income
+    transactions.income.sum(:amount)
+  end
+
+  def total_expenses
+    transactions.expense.sum(:amount)
+  end
+
+  def remaining_balance
+    total_income - total_expenses
+  end
 end
